be84dbdc5023e886bfcca07ee1229592893afd23
Madenoglu, Beyza, Vodafone (External) <Beyza.Madenoglu@vodafone.com>
2022-10-03 11:12:49 +0300

    route added

--- src/Router/router.js
+++ src/Router/router.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Default from '../pages/DefaultPage';
-import Status from '../components/Statusbar';
-import Profile from '../components/Profile';
-import Publishes from '../components/Publishes';
+import FeedPage from '../pages/FeedPage';
+
 
 function Router() {
     return (
         <>
             <Routes>
-                <Route path="/" element={<Default />} />
-                <Route path="/feed" element={
-                    <>
-                        <Status />
-                        <Profile />
-                        <Publishes />
-                    </>
-                } />
+                <Route path="/" element={<FeedPage />} />
+                <Route path="/feed" element={<FeedPage />} />
                 <Route path="*" element={<Default />} />
 
             </Routes>
---
+++ src/pages/FeedPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Status from '../components/Statusbar';
+import Profile from '../components/Profile';
+import Publishes from '../components/Publishes';
+
+function FeedPage() {
+  return (
+    <><Status />
+    <Profile />
+    <Publishes /></>
+  )
+}
+
+export default FeedPage
